c3a21459fd336196ce177408de938a29e7fa7f54
Gabriel Silva <gsg.gabrielsilva96@gmail.com>
2024-02-27 19:48:12 -0300

    Subindo a minha pokedex, sem fazer a att dos status ainda

---
+++ assets/js/pokemon-model.js
@@ -0,0 +1,10 @@
+/* Esse sera as configurações dos nossos pokemons */
+/* Chamasse "responsabilidade da API", no casso, nossa configuração para a api que pegamos de fora   */
+class Pokemon {
+    number;
+    name;
+    type;
+    types = [];
+    photo;
+    status;
+}
